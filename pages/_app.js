d687bd0a836af919027fd9323c2a83e06d58b60b
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-03-31 19:32:29 +0700

    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
 import '@/styles/globals.css'
+import { Roboto_Flex, Titillium_Web } from 'next/font/google'
+
+const roboto = Roboto_Flex({ subsets: ['latin'] })
+const titillium = Titillium_Web({
+  subsets: ['latin'],
+  weight: ["400", "900"],
+  fallback: "sans-serif"
+})
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <main className={`${titillium.className}`}>
+  <Component {...pageProps} />
+  </main>
 }
