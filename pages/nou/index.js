78f6be749a53e2ab9693b85405481bfdab154499
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-04-21 17:32:10 +0700

    feature sections

--- pages/nou/index.js
+++ pages/nou/index.js
@@ -43,32 +43,59 @@ function nou() {
 							<h3 className={utils.aside__title}>Project T.L.D.R</h3>
 							<p className={utils.aside__info}>
 								This site was created for a branding and web design studio. It's
-								built with React / Next.js, showcasing reponsive design, animations
-								including the web animations API, forms with regex validation and
-								email submission. 
+								built with React / Next.js, showcasing reponsive design,
+								animations including the web animations API, forms with regex
+								validation and email submission.
 							</p>
 							<div className={utils.aside__keyword}>
-                                <ul className={utils.keyword__list}>
-                                    <li>React</li>
-                                    <li>Next.js</li>
-                                    <li>Responsive Design</li>
-                                    <li>WAAPI</li>
-                                    <li>Intersection Observer</li>
-                                    <li>RegEx</li>
-                                    <li>Node mailer</li>
-                                </ul>
-                            </div>
-                            <div className={utils.aside__btns}>
-                                <button className={utils.btn}>Live Site</button>
-                                <button className={utils.btn}>Source Code</button>
-                            </div>
+								<ul className={utils.keyword__list}>
+									<li>React</li>
+									<li>Next.js</li>
+									<li>Responsive Design</li>
+									<li>WAAPI</li>
+									<li>Intersection Observer</li>
+									<li>RegEx</li>
+									<li>Node mailer</li>
+								</ul>
+							</div>
+							<div className={utils.aside__btns}>
+								<button className={utils.btn}>Live Site</button>
+								<button className={utils.btn}>Source Code</button>
+							</div>
 						</aside>
 					</section>
 
-                    <section className={utils.planning_section}>
-                        <h2>Purpose &amp; Planning</h2>
+					<section className={utils.planning_section}>
+						<h2>Purpose &amp; Planning</h2>
+						<p className={utils.planning_section__purpose}>
+							This project was made to market and showcase projects to potential
+							clients of the business. It lists the services, creatively
+							displays previous projects and provides a form for enquiries
+						</p>
+                        {/* <Image src={} alt='Adobe XD planning pictures' /> */}
 
-                    </section>
+						<p className={utils.planning_section__planning}>
+							I utilised the clients designs from Adobe XD, to create a mobile
+							first webpage that is responsive for tablet and desktops. Workflow
+							was tracked with use of a Kanban board to outline an MVP and then
+							break down goals into actionable tasks. While Version control was managed with GitHub,
+                            including feature branches to avoid breaking commits to the main branch. 
+						</p>
+                        {/* <Image src={} alt='Trello Kanban board picture' /> */}
+
+					</section>
+
+					<section className={utils.challenges}>
+						<h2>Challenges and wins</h2>
+					</section>
+
+					<section className={utils.stack}>
+						<h2>Stack &amp; Features</h2>
+					</section>
+
+					<section className={utils.conclusion}>
+						<h2>What I learned</h2>
+					</section>
 				</main>
 
 				<footer className={utils.contact}>
