0789572ecd6fea32a10ea69b13c5e04286863591
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-05-03 00:19:32 +0700

    project page layout

--- pages/nou/index.js
+++ pages/nou/index.js
@@ -5,8 +5,27 @@ import styles from "../../styles/Nou.module.css";
 import Image from "next/image";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { SiCss3, SiJavascript, SiReact, SiGit } from "react-icons/si";
+import { data } from "../data";
 
 function nou() {
+
+    const projectImages = data.map((project) => {
+        if (project.id === 'NOU') {
+            return project.images.map((img, index) => {
+                return(
+                    <Image
+								className={utils.flex__image}
+								src={img}
+								alt={`${project.name} picture`}
+								height={600}
+								width={800}
+                                key={index}
+							/>
+                )
+            } )
+        }
+    })
+
 	return (
 		<>
 			<Head>
@@ -30,7 +49,7 @@ function nou() {
 						<h2 className={utils.title_primary}>Nou Design Studio</h2>
 					</section>
 
-					<section className={utils.hero_section}>
+					<section className={`${utils.hero_section} ${utils.sections}`}>
 						<div className={utils.hero_section__main}>
 							<Image
 								className={utils.main__img}
@@ -45,8 +64,8 @@ function nou() {
 							<p className={utils.aside__info}>
 								This site was created for a branding and web design studio. It's
 								built with React / Next.js, showcasing reponsive design,
-								animations including the web animations API, forms with regex
-								validation and email submission.
+								animations including parallax scroll, web animations API, forms
+								with validation and email submission.
 							</p>
 							<div className={utils.aside__keyword}>
 								<ul className={utils.keyword__list}>
@@ -55,6 +74,7 @@ function nou() {
 									<li>Responsive Design</li>
 									<li>WAAPI</li>
 									<li>Intersection Observer</li>
+									<li>Parallax</li>
 									<li>RegEx</li>
 									<li>Node mailer</li>
 								</ul>
@@ -69,7 +89,7 @@ function nou() {
 								</a>
 								<a
 									className={utils.link}
-									href='https://nou-ng2gsrpkw-pilks-pixel.vercel.app/'
+									href='https://noudesignstudio.vercel.app'
 									target='_blank'
 								>
 									Live Site
@@ -78,109 +98,142 @@ function nou() {
 						</aside>
 					</section>
 
-					<section className={utils.planning_section}>
-                        <h2>Purpose &amp; Planning</h2>
-                        <div className={utils.section_grid}>
-                            <p className={utils.planning_section__purpose}>
-                                This project was made to market and showcase projects to potential
-                                clients of the business. It lists the services, creatively
-                                displays previous projects and provides a form for enquiries
-                            </p>
-                            <Image
-                                className={utils.planning_section__image}
-                                src="/adobe_screenshot.png"
-                                alt='Trello Kanban board picture'
-                                height={600}
-                                width={800}
-                            />
+					<section className={`${utils.planning_section} ${utils.sections}`}>
+						<h2>Purpose &amp; Planning</h2>
+						<div className={utils.section_grid}>
+							<p className={utils.planning_section__purpose}>
+								This project was made to market and showcase projects to
+								potential clients of the business. It lists the services,
+								creatively displays previous projects and provides a form for
+								enquiries
+							</p>
+							<Image
+								className={utils.planning_section__image}
+								src='/adobe_screenshot.png'
+								alt='Trello Kanban board picture'
+								height={600}
+								width={800}
+							/>
 
-                            <p className={utils.planning_section__planning}>
-                                I&apos;ve utilised the clients designs from Adobe XD, to create a
-                                mobile first webpage that is responsive for tablet and desktops.
-                                Workflow was managed with use of a Kanban board to outline an MVP
-                                and then break down goals into actionable tasks. While Version
-                                control was managed with GitHub, including feature branches to
-                                avoid breaking commits to the main branch.
-                            </p>
-                            <Image
-                                className={utils.planning_section__image}
-                                src="/trello_screenshot.png"
-                                alt='Trello Kanban board picture'
-                                height={600}
-                                width={800}
-                            />
-                        </div>
+							<p className={utils.planning_section__planning}>
+								I&apos;ve worked to the clients designs from Adobe XD, to create
+								a webpage that is fully responsive. Workflow was managed with
+								use of a Kanban board to outline an MVP and then break down
+								goals into actionable tasks. While Version control was managed
+								with GitHub, including feature branches to avoid breaking
+								commits to the main branch.
+							</p>
+							<Image
+								className={utils.planning_section__image}
+								src='/trello_screenshot.png'
+								alt='Trello Kanban board picture'
+								height={600}
+								width={800}
+							/>
+						</div>
 					</section>
 
-					<section className={utils.stack}>
+					<section className={`${utils.stack} ${utils.sections}`}>
 						<h2>Stack &amp; Features</h2>
-                        <div className={utils.section_grid}>
-                            <div className={utils.stack__icons}>
-                                <ul className={utils.icons__list}>
-                                    <li>
-                                        <SiReact color='#61DAFB' size={42} /> React
-                                    </li>
-                                    <li>
-                                        <Image
-                                            src='/next.svg'
-                                            alt='Next.js icon'
-                                            width={60}
-                                            height={60}
-                                        />
-                                        Next.js
-                                    </li>
-                                    <li>
-                                        <SiCss3 color='#1572B6' size={42} />
-                                        CSS
-                                    </li>
-                                    <li>
-                                        <SiJavascript color='#F0DB4F' size={42} />
-                                        Javascript ES6
-                                    </li>
-                                    <li>
-                                        <Image
-                                            src='/vercel.svg'
-                                            alt='Next.js icon'
-                                            width={60}
-                                            height={60}
-                                        />
-                                        Vercel
-                                    </li>
-                                </ul>
-                            </div>
+						<div className={utils.section_grid}>
+							<div className={utils.stack__icons}>
+								<ul className={utils.icons__list}>
+									<li>
+										<SiReact color='#61DAFB' size={42} /> React
+									</li>
+									<li>
+										<Image
+											src='/next.svg'
+											alt='Next.js icon'
+											width={60}
+											height={60}
+										/>
+										Next.js
+									</li>
+									<li>
+										<SiCss3 color='#1572B6' size={42} />
+										CSS
+									</li>
+									<li>
+										<SiJavascript color='#F0DB4F' size={42} />
+										Javascript ES6
+									</li>
+									<li>
+										<Image
+											src='/vercel.svg'
+											alt='Next.js icon'
+											width={60}
+											height={60}
+										/>
+										Vercel
+									</li>
+								</ul>
+							</div>
 
-                            <div className={utils.stack__features}>
-                                <h3 className={utils.features_heading}>Stack</h3>
-                                <p>
-                                    This website is built with React and the Next.js meta framework.
-                                    Next enabled optimised development and deployment features like
-                                    automatic code spliting, lazy loaded images and improved SEO.
-                                    Allowing me to deliver a performant website with excellent user
-                                    experience.
-                                    <br></br>
-                                    Next also meant Vercel was used for deployment, which meant I
-                                    could utilise seprate production and feature deployments
-                                    alongside version control.
-                                </p>
-
-                                <h3 className={utils.features_heading}>Features</h3>
-                                <p>
-                                    Animations were used to add fliar to the site with transitions
-                                    and CSS pseudo elements. I also chose to use an the web
-                                    animation API with an intersection observer which allowed
-                                    animation only when entering the viewport, giving a more
-                                    polished UI and improved perfomance.
-                                </p>
-                            </div>
-                        </div>
+							<div className={utils.stack__features}>
+								<h2 className={utils.features__heading}>Stack</h2>
+								<p>
+									This website is built with React and the Next.js meta
+									framework. Next enabled optimised development and deployment
+									features like automatic code spliting, lazy loaded images and
+									improved SEO. Allowing me to deliver a performant website with
+									excellent user experience.
+									<br></br>
+									Next also meant Vercel was used for deployment, which meant I
+									could utilise seprate production and feature deployments
+									alongside version control.
+								</p>
+							</div>
+						</div>
 					</section>
 
-					<section className={utils.challenges}>
-						<h2>Challenges and wins</h2>
+					<section className={`${utils.challenges} ${utils.sections}`}>
+						<div className={utils.features__flex}>
+                            {projectImages}
+						</div>
+						<h2>Features &amp; Challenges</h2>
+						<h3>Animations</h3>
+						<p>
+							Parallax scroll was used with CSS transitions to add fliar to the
+							site. I also chose to use an the web animation API with an
+							intersection observer which allowed animation according to
+							viewport position, giving a more polished UI and improved
+							perfomance.
+						</p>
+						<p>
+							This was tricky as I wanted different elements to fade in at
+							staggered timings, so had to use callback functions to create an
+							array of Refs and iterate over in the observer. This allowed me to
+							animate elements based on position in the viewport and apply
+							delays based on element tag names.
+						</p>
+
+						<h3>Responsive layouts</h3>
+						<p>
+							Getting the site to be responsive at all screen sizes, meant
+							needing to utilize layout tools like Grid and Flexbox. Media
+							queries were also useful to define breakpoints but I tried to
+							implement modern CSS functions like clamp in order to give
+							responsive text without media queries.
+						</p>
+						<h3>Contact Form</h3>
+						<p>
+							The useState hook allowed a controlled form with validation for
+							user input, including Regex pattern for email inputs. I was able
+							to make use of the in built next js API. This allowed me to handle
+							form submission of user data with the node mailer library.
+						</p>
 					</section>
 
-					<section className={utils.conclusion}>
+					<section className={`${utils.conclusion} ${utils.sections}`}>
 						<h2>What I learned</h2>
+						<p>
+							This project enabled me to learn how to create smooth animations
+							according to uses scroll position. I also learned how to handle
+							user inputs with email submission through node mailer and
+							increased my understanding of responsive design with layout tools
+							like CSS grid.
+						</p>
 					</section>
 				</main>
 
