773fda1514c8759de57581204e7d1f9fd2f2974c
Pilks-pixel <peterpilkington@hotmail.com>
2023-05-04 13:37:00 +0700

    text format

--- pages/nou/index.js
+++ pages/nou/index.js
@@ -8,23 +8,22 @@ import { SiCss3, SiJavascript, SiReact, SiGit } from "react-icons/si";
 import { data } from "../data";
 
 function nou() {
-
-    const projectImages = data.map((project) => {
-        if (project.id === 'NOU') {
-            return project.images.map((img, index) => {
-                return(
-                    <Image
-								className={utils.flex__image}
-								src={img}
-								alt={`${project.name} picture`}
-								height={600}
-								width={800}
-                                key={index}
-							/>
-                )
-            } )
-        }
-    })
+	const projectImages = data.map(project => {
+		if (project.id === "NOU") {
+			return project.images.map((img, index) => {
+				return (
+					<Image
+						className={utils.flex__image}
+						src={img}
+						alt={`${project.name} picture`}
+						height={600}
+						width={800}
+						key={index}
+					/>
+				);
+			});
+		}
+	});
 
 	return (
 		<>
@@ -139,10 +138,10 @@ function nou() {
 						<div className={utils.section_grid}>
 							<div className={utils.stack__icons}>
 								<ul className={utils.icons__list}>
-									<li>
+									<li className={utils.list__icon}>
 										<SiReact color='#61DAFB' size={42} /> React
 									</li>
-									<li>
+									<li className={`${utils.list__icon} ${utils.list_img}`}>
 										<Image
 											src='/next.svg'
 											alt='Next.js icon'
@@ -151,15 +150,15 @@ function nou() {
 										/>
 										Next.js
 									</li>
-									<li>
+									<li className={utils.list__icon}>
 										<SiCss3 color='#1572B6' size={42} />
 										CSS
 									</li>
-									<li>
+									<li className={utils.list__icon}>
 										<SiJavascript color='#F0DB4F' size={42} />
 										Javascript ES6
 									</li>
-									<li>
+									<li className={`${utils.list__icon} ${utils.list_img}`}>
 										<Image
 											src='/vercel.svg'
 											alt='Next.js icon'
@@ -189,40 +188,46 @@ function nou() {
 
 					<section className={`${utils.challenges} ${utils.sections}`}>
 						<h2>Features &amp; Challenges</h2>
+						<div className={utils.features__flex}>{projectImages}</div>
 						<div className={utils.features__flex}>
-                            {projectImages}
+							<div>
+								<h3>Animations</h3>
+								<p>
+									Parallax scroll was used with CSS transitions to add fliar to
+									the site. I also chose to use an the web animation API with an
+									intersection observer which allowed animation according to
+									viewport position, giving a more polished UI and improved
+									perfomance.
+								</p>
+								<p>
+									This was tricky as I wanted different elements to fade in at
+									array of Refs and iterate over in the observer. This allowed
+									me to animate elements based on position in the viewport and
+									apply delays based on element tag names.
+								</p>
+							</div>
+							<div>
+								<h3>Responsive layouts</h3>
+								<p>
+									Getting the site to be responsive at all screen sizes, meant
+									needing to utilize layout tools like Grid and Flexbox. Media
+									queries were also useful to define breakpoints but I tried to
+									implement modern CSS functions like clamp in order to give
+									staggered timings, so had to use callback functions to create
+									an responsive text without media queries.
+								</p>
+							</div>
+							<div>
+								<h3>Contact Form</h3>
+								<p>
+									The useState hook allowed a controlled form with validation
+									for user input, including Regex pattern for email inputs. I
+									was able to make use of the in built next js API. This allowed
+									me to handle form submission of user data with the node mailer
+									library.
+								</p>
+							</div>
 						</div>
-						<h3>Animations</h3>
-						<p>
-							Parallax scroll was used with CSS transitions to add fliar to the
-							site. I also chose to use an the web animation API with an
-							intersection observer which allowed animation according to
-							viewport position, giving a more polished UI and improved
-							perfomance.
-						</p>
-						<p>
-							This was tricky as I wanted different elements to fade in at
-							array of Refs and iterate over in the observer. This allowed me to
-							animate elements based on position in the viewport and apply
-							delays based on element tag names.
-						</p>
-
-						<h3>Responsive layouts</h3>
-						<p>
-							Getting the site to be responsive at all screen sizes, meant
-							needing to utilize layout tools like Grid and Flexbox. Media
-							queries were also useful to define breakpoints but I tried to
-							implement modern CSS functions like clamp in order to give
-							staggered timings, so had to use callback functions to create an
-							responsive text without media queries.
-						</p>
-						<h3>Contact Form</h3>
-						<p>
-							The useState hook allowed a controlled form with validation for
-							user input, including Regex pattern for email inputs. I was able
-							to make use of the in built next js API. This allowed me to handle
-							form submission of user data with the node mailer library.
-						</p>
 					</section>
 
 					<section className={`${utils.conclusion} ${utils.sections}`}>
