409bbf09d562bbc02e48e1d684ccabdc40a189df
Pilks-pixel <peterpilkington@hotmail.com>
2023-08-31 15:22:04 +0200

    favicon

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -133,7 +133,24 @@ function PixChat() {
 					content='peter pilkington junior software developer portfolio'
 				/>
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
-				<link rel='icon' href='/favicon.ico' />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='favicon_io/apple-touch-icon.png'
+				></link>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='32x32'
+					href='favicon_io/favicon-32x32.png'
+				></link>
+				<link
+					rel='icon'
+					type='image/png'
+					sizes='16x16'
+					href='favicon_io/favicon-16x16.png'
+				></link>
+				<link rel='manifest' href='favicon_io/site.webmanifest'></link>
 			</Head>
 
 			<div className={utils.wrapper}>
@@ -431,8 +448,8 @@ function PixChat() {
 								<p>
 									Making API routes for C.R.U.D functionality among users /
 									messages, storing them in a database, and retrieving message
-									history between users, strengthened my knowledge of RESTful API
-									principles and HTTP requests using the Axios library.
+									history between users, strengthened my knowledge of RESTful
+									API principles and HTTP requests using the Axios library.
 								</p>
 								<p>
 									Additionally I improved my knowledge of accessibility, testing
